e449a57f7b2f4fcd5a2ba1574bd0de276c9607bd
Sachin Jaiswal <sachin.jaiswal@techdome.net.in>
2024-02-15 13:26:08 +0530

    classrooom

--- program1.js
+++ program1.js
@@ -1,11 +1,27 @@
+
+
 /**
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+let isValid = function (s) {
+    let map = new Map();
+    map.set('{', '}');
+    map.set('(', ')');
+    map.set('[', ']');
+    let b = [];
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s.charAt(i))) {
+            b.push(s.charAt(i));
+        }
+        else {
+            let pop = b.pop();
+            if (map.get(pop) !== s.charAt(i)) {
+                return false;
+            }
+        }
+    }
+    return b.length === 0;
 };
 
 module.exports = { isValid };
-
-
